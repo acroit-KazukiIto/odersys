708871dc631fcecaea96950f7046515fa83e884d
秋嶋琉太 <@AC2026-80-002>
2026-05-12 18:02:23 +0900

    update:OrderListServlet

--- src/servlet/OrderListServlet.java
+++ src/servlet/OrderListServlet.java
@@ -2,11 +2,13 @@ package servlet;
 
 import java.io.IOException;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import model.OrderList;
 
 @WebServlet("/OrderListServlet")
 public class OrderListServlet extends HttpServlet {
@@ -20,14 +22,32 @@ public class OrderListServlet extends HttpServlet {
 
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		//リクエストパラメータの文字コード指定
+		request.setCharacterEncoding("UTF-8");
 		//リクエストパラメータの取得
 		String productName = request.getParameter("productName");
 		String toppingName = request.getParameter("toppingName");
-		int productPrice = Integer.parseInt(productPrice);
-		int toppingPrice = Integer.parseInt(toppingPrice);
-		int  = request.getParameter();
-		int  = request.getParameter();
-		int  = request.getParameter();
+		String productPrice = request.getParameter("productPrice");
+		String toppingPrice = request.getParameter("toppingPrice");
+		String toppingQuantity = request.getParameter("toppingQuantity");
+		String subTotal = request.getParameter("subTotal");
+		String productTopping  = request.getParameter("productTopping");
+		
+		//入力値をプロパティに設定
+		OrderList ol = new OrderList();
+		ol.setProductName(productName);
+		ol.setToppingName(toppingName);
+		ol.setProductPrice(productPrice);
+		ol.setToppingPrice(toppingPrice);
+		ol.();
+		ol.();
+		ol.();
+		
+		//フォーワード
+		RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/jsp/orderList.jsp");
+		dispatcher.forward(request, response);
+		
 	}
 
 }
