78b84ee333faba05b3b6c9501e67fb184687c768
石井満雄 <@AC2026-80-003>
2026-05-13 15:17:21 +0900

    Merge branch 'master' of https://github.com/acroit-KazukiIto/odersys

--- src/model/ProductInfo.java
+++ src/model/ProductInfo.java
@@ -9,69 +9,23 @@ public class ProductInfo {
     private int productStock;
     private int productDisplayFlag;
 
-    public ProductInfo() {
-    }
+    public ProductInfo() {}
 
-    public ProductInfo(int productId,
-            String productName,
-            String categoryName,
-            int productPrice,
-            int productStock,
-            int productDisplayFlag) {
+    public int getProductId() { return productId; }
+    public void setProductId(int productId) { this.productId = productId; }
 
-        this.productId = productId;
-        this.productName = productName;
-        this.categoryName = categoryName;
-        this.productPrice = productPrice;
-        this.productStock = productStock;
-        this.productDisplayFlag = productDisplayFlag;
-    }
+    public String getProductName() { return productName; }
+    public void setProductName(String productName) { this.productName = productName; }
 
-    public int getProductId() {
-        return productId;
-    }
+    public String getCategoryName() { return categoryName; }
+    public void setCategoryName(String categoryName) { this.categoryName = categoryName; }
 
-    public void setProductId(int productId) {
-        this.productId = productId;
-    }
+    public int getProductPrice() { return productPrice; }
+    public void setProductPrice(int productPrice) { this.productPrice = productPrice; }
 
-    public String getProductName() {
-        return productName;
-    }
+    public int getProductStock() { return productStock; }
+    public void setProductStock(int productStock) { this.productStock = productStock; }
 
-    public void setProductName(String productName) {
-        this.productName = productName;
-    }
-
-    public String getCategoryName() {
-        return categoryName;
-    }
-
-    public void setCategoryName(String categoryName) {
-        this.categoryName = categoryName;
-    }
-
-    public int getProductPrice() {
-        return productPrice;
-    }
-
-    public void setProductPrice(int productPrice) {
-        this.productPrice = productPrice;
-    }
-
-    public int getProductStock() {
-        return productStock;
-    }
-
-    public void setProductStock(int productStock) {
-        this.productStock = productStock;
-    }
-
-    public int getProductDisplayFlag() {
-        return productDisplayFlag;
-    }
-
-    public void setProductDisplayFlag(int productDisplayFlag) {
-        this.productDisplayFlag = productDisplayFlag;
-    }
+    public int getProductDisplayFlag() { return productDisplayFlag; }
+    public void setProductDisplayFlag(int productDisplayFlag) { this.productDisplayFlag = productDisplayFlag; }
 }
