fa9c5af7df27604074a89104c6d37f561861e240
伊藤和樹 <@AC2026-80-004>
2026-05-22 13:43:14 +0900

    update:会計完了画面

--- src/servlet/CheckOutServlet.java
+++ src/servlet/CheckOutServlet.java
@@ -16,12 +16,10 @@ public class CheckOutServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    	System.out.println("doPostに入りました");
         // 入力データの取得
         String tableNumber = request.getParameter("tableNumber");
         String totalPriceStr = request.getParameter("totalOrderPrice");
         int totalOrderPrice = Integer.parseInt(totalPriceStr);
-        System.out.println("入力データの取得");
 
         // データベースの更新
         CheckOutDAO dao = new CheckOutDAO();
@@ -38,17 +36,14 @@ public class CheckOutServlet extends HttpServlet {
         // jspへの出力データのセット
         CheckOutInfo info = new CheckOutInfo(tableNumber, totalOrderPrice);
         request.setAttribute("checkOutInfo", info);
-        System.out.println("jspへの出力データをセットを行いました");
-
+        
         // jspへフォワード
         request.getRequestDispatcher("/WEB-INF/jsp/checkOut.jsp").forward(request, response);
-        System.out.println("jspへフォワードしました");
     }
 
     protected void doGet(HttpServletRequest request,
     		HttpServletResponse response)
     				throws ServletException, IOException {
-    	System.out.println("doPostに入りました");
         doPost(request, response);
     }
 }
